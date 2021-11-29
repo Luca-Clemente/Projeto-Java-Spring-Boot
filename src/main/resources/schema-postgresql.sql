6f31f3473a512f1bb7fd631165a934ad7cdcec2e
Luca Clemente <72042178+Luca-Clemente@users.noreply.github.com>
2021-11-28 21:25:55 -0300

    update schema-postgresql

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -4,3 +4,19 @@ CREATE TABLE IF NOT EXISTS obra (
      ano   varchar(20),
      autor varchar(20)
 );
+CREATE TABLE IF NOT EXISTS compradores (
+	id serial PRIMARY KEY,
+	nome varchar(50),
+	email varchar(20)
+);
+CREATE TABLE IF NOT EXISTS funcionarios (
+	id serial PRIMARY KEY,
+	nome varchar(50),
+	cargo varchar (20)
+);
+CREATE TABLE IF NOT EXISTS materiais(
+	id serial PRIMARY KEY,
+	nome varchar(50),
+	quantidade int
+	
+);
